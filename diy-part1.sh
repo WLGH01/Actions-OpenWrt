38712314587c6c92f906811f3a456c4abc7dc445
WLGH01 <113486817+WLGH01@users.noreply.github.com>
2025-04-19 14:55:56 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,9 +14,17 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-echo 'src-git modem https://github.com/siriling/5G-Modem-Support' >>feeds.conf.default
-echo 'src-git qmodem https://github.com/FUjr/modem_feeds.git;main' >>feeds.conf.default
+sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '3i src-git-full hellworld https://github.com/fw876/helloworld.git' feeds.conf.default
+sed -i '4i src-git-full modem https://github.com/Siriling/5G-Modem-Support.git' feeds.conf.default
+sed -i '5i src-git-full clash https://github.com/vernesong/OpenClash.git' feeds.conf.default
+sed -i '6i src-git-full passwall https://github.com/xiaorouji/openwrt-passwall-packages.git' feeds.conf.default
+sed -i '7i src-git qmodem https://github.com/FUjr/modem_feeds.git;main' feeds.conf.default
+./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns
+rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
+rm -rf feeds/packages/utils/v2dat
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+./scripts/feeds install -a 
+sed -i '3i echo 50 > /sys/class/hwmon/hwmon1/pwm1' package/base-files/files/etc/rc.local
