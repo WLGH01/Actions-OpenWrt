3b34ac3b0ad3f7e739a5ef1b08c98eab960cbc33
WLGH01 <113486817+WLGH01@users.noreply.github.com>
2026-08-24 12:53:36 +0000

    fix: use valid daed branch and package BTF directly

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,31 +1,20 @@
 #!/bin/bash
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
+# OpenWrt DIY script part 1: customize feeds before feed update.
+set -e
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
-# Add a feed source
+# Additional third-party feeds used by the current configuration.
 sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 sed -i '3i src-git-full hellworld https://github.com/fw876/helloworld.git' feeds.conf.default
 sed -i '4i src-git-full modem https://github.com/Siriling/5G-Modem-Support.git' feeds.conf.default
-# sed -i '5i src-git-full clash https://github.com/vernesong/OpenClash.git' feeds.conf.default
-sed -i '6i src-git-full passwall https://github.com/xiaorouji/openwrt-passwall-packages.git' feeds.conf.default
-sed -i '7i src-git qmodem https://github.com/FUjr/modem_feeds.git;main' feeds.conf.default
-./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns
-rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
-rm -rf feeds/packages/utils/v2dat
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
-./scripts/feeds install -a 
-sed -i '3i echo 50 > /sys/class/hwmon/hwmon1/pwm1' package/base-files/files/etc/rc.local
+sed -i '5i src-git-full passwall https://github.com/xiaorouji/openwrt-passwall-packages.git' feeds.conf.default
+sed -i '6i src-git qmodem https://github.com/FUjr/modem_feeds.git;main' feeds.conf.default
 
+# QiuSimons/vmlinux-btf is a standalone package repository, not a normal feed.
+# A normal feed requires package subdirectories and therefore produces a broken
+# .files-packageinfo.mk when this repository is registered as src-git.
+rm -rf package/vmlinux-btf
+git clone --depth=1 --single-branch --branch master \
+  https://github.com/QiuSimons/vmlinux-btf.git package/vmlinux-btf
+
+# Do not add ARM-specific PWM commands: this build targets x86_64.
