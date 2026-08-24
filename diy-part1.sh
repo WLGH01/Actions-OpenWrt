b00d19160a53b1fda947dddb8295c3bef2da2764
WLGH01 <113486817+WLGH01@users.noreply.github.com>
2026-08-24 16:24:29 +0000

    fix: align small feed and remove incompatible modem packages

--- diy-part1.sh
+++ diy-part1.sh
@@ -2,19 +2,23 @@
 # OpenWrt DIY script part 1: customize feeds before feed update.
 set -e
 
-# Additional third-party feeds used by the current configuration.
+# Third-party feeds required by the current x86_64 configuration.
 sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
-sed -i '3i src-git-full hellworld https://github.com/fw876/helloworld.git' feeds.conf.default
-sed -i '4i src-git-full modem https://github.com/Siriling/5G-Modem-Support.git' feeds.conf.default
-sed -i '5i src-git-full passwall https://github.com/xiaorouji/openwrt-passwall-packages.git' feeds.conf.default
-sed -i '6i src-git qmodem https://github.com/FUjr/modem_feeds.git;main' feeds.conf.default
 
-# QiuSimons/vmlinux-btf is a standalone package repository, not a normal feed.
-# A normal feed requires package subdirectories and therefore produces a broken
-# .files-packageinfo.mk when this repository is registered as src-git.
+# Remove packages replaced by the kenzok8/small feed, then use its
+# matching Go toolchain as documented by the feed.
+./scripts/feeds update -a
+rm -rf feeds/luci/applications/luci-app-mosdns
+rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,sing*,smartdns}
+rm -rf feeds/packages/utils/v2dat
+rm -rf feeds/packages/lang/golang
+git clone --depth=1 --single-branch --branch 1.26 \
+  https://github.com/kenzok8/golang feeds/packages/lang/golang
+
+# vmlinux-btf is a standalone package repository, not a normal feed.
 rm -rf package/vmlinux-btf
 git clone --depth=1 --single-branch --branch master \
   https://github.com/QiuSimons/vmlinux-btf.git package/vmlinux-btf
 
-# Do not add ARM-specific PWM commands: this build targets x86_64.
+# Do not add ARM/5G-specific feeds or PWM commands to this x86_64 build.
